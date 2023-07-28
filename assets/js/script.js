5019a5c25fd7407f545df58aa26c6df8c29e1562
oladokunlt <lukmanoladokun86@gmail.com>
2023-07-28 16:16:42 +0100

    Rendered country data from api for homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const modeIcon = document.querySelector("#mode-icon");
 const modeName = document.querySelector("#mode-name");
 const modeBtn = document.querySelector("#mode-name");
+const countries = document.querySelector(".countries")
 
 modeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
@@ -11,6 +12,39 @@ modeBtn.addEventListener("click", () => {
         modeIcon.textContent = "dark_mode";
         modeName.textContent = "Dark Mode";
     }
-})
+});
+
+async function getData() {
+    const jsonData = await fetch("../data.json")
+    const res = await jsonData.json()
+    console.log(res)
+    res.forEach(e => {
+        handleShowCountry(e);
+    });
+    
+}
+getData()
+
+function handleShowCountry(ctrData) {
+    const countryCard = document.createElement("div")
+    countryCard.classList.add("country-card")
+    countryCard.innerHTML = 
+    `
+    <div class="flag-wrapper">
+        <img src="${ctrData.flag}" alt="">
+    </div>
+    <div class="demographic-wrapper">
+        <h2 class="country-name">${ctrData.name}</h2>
+        <p><strong>Population:</strong> <span>${ctrData.population}</span></p>
+        <p><strong>Population:</strong> <span>${ctrData.region}</span></p>
+        <p><strong>Population:</strong> <span> ${ctrData.capital} </span></p>
+    </div>
+    `
+    countries.append(countryCard)
+    // ctrData.forEach((e) => {
+    //     console.log(e.flag)
+    // });
+}
+
 
  
